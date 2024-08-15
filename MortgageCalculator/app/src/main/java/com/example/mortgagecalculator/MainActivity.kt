74bd103fed40e78406ba7f21403d26113d4efab0
rledezma1 <redgem8170@gmail.com>
2024-08-14 22:29:12 -0700

    Added the Sign_On page

--- MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
+++ MortgageCalculator/app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
@@ -19,16 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            MortgageCalculatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_sign_on)
     }
 }
 
